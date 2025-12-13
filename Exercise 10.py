ea1a14d3c1acc79d2488ebc9e34c36cd14e2e3a9
Sneha Saha <souvik9874721150@gmail.com>
2025-12-13 21:40:00 +0530

    How to generate custom sequences in numpy without hardcoding?

---
+++ Exercise 10.py
@@ -0,0 +1,9 @@
+#  How to generate custom sequences in numpy without hardcoding?
+
+import numpy as np 
+
+seq = np.arange(5, 30, 5) 
+print(seq)     # => [ 5 10 15 20 25]
+
+a = np.array([1,2,3])
+print(np.r_[np.repeat(a, 3), np.tile(a, 3)])     # => [1 1 1 2 2 2 3 3 3 1 2 3 1 2 3 1 2 3]
